f923bf0d4ce8e96a706336e03d6e547d46a18bde
Ryan Kowalewski <ryan@ryanjan.uk>
2019-12-23 16:29:02 +0000

    Improve handling zero updates returned.

--- MSCatalog/Public/Get-MSCatalogUpdate.ps1
+++ MSCatalog/Public/Get-MSCatalogUpdate.ps1
@@ -65,40 +65,55 @@ function Get-MSCatalogUpdate {
         [String] $ViewStateGenerator
     )
 
-    if ($Method -eq "Post") {
-        $ReqBody = @{
-            "__EVENTARGUMENT" = $EventArgument
-            "__EVENTTARGET" = $EventTarget
-            "__EVENTVALIDATION" = $EventValidation
-            "__VIEWSTATE" = $ViewState
-            "__VIEWSTATEGENERATOR" = $ViewStateGenerator
+    try {
+
+        if ($Method -eq "Post") {
+            $ReqBody = @{
+                "__EVENTARGUMENT" = $EventArgument
+                "__EVENTTARGET" = $EventTarget
+                "__EVENTVALIDATION" = $EventValidation
+                "__VIEWSTATE" = $ViewState
+                "__VIEWSTATEGENERATOR" = $ViewStateGenerator
+            }
         }
-    }
-    $UriSearch = [Uri]::EscapeUriString($Search)
-    $Params = @{
-        Uri = "https://www.catalog.update.microsoft.com/Search.aspx?q=$UriSearch"
-        Method = $Method
-        Body = $ReqBody
-        ContentType = "application/x-www-form-urlencoded"
-        UseBasicParsing = $true
-    }
-    $Results = Invoke-WebRequest @Params
+        $UriSearch = [Uri]::EscapeUriString($Search)
+        $Params = @{
+            Uri = "https://www.catalog.update.microsoft.com/Search.aspx?q=$UriSearch"
+            Method = $Method
+            Body = $ReqBody
+            ContentType = "application/x-www-form-urlencoded"
+            UseBasicParsing = $true
+        }
+        $Results = Invoke-WebRequest @Params
 
-    $HtmlDoc = [HtmlAgilityPack.HtmlDocument]::new()
-    $HtmlDoc.LoadHtml($Results.RawContent.ToString())
-    $NextPage = $HtmlDoc.GetElementbyId("ctl00_catalogBody_nextPage")
-    $EventArgument = $HtmlDoc.GetElementbyId("__EVENTARGUMENT")[0].Attributes["value"].Value
-    $EventValidation = $HtmlDoc.GetElementbyId("__EVENTVALIDATION")[0].Attributes["value"].Value
-    $ViewState = $HtmlDoc.GetElementbyId("__VIEWSTATE")[0].Attributes["value"].Value
-    $ViewStateGenerator = $HtmlDoc.GetElementbyId("__VIEWSTATEGENERATOR")[0].Attributes["value"].Value
-    $Table = $HtmlDoc.GetElementbyId("ctl00_catalogBody_updateMatches")
-    $Rows = $Table.SelectNodes("tr")
+        $HtmlDoc = [HtmlAgilityPack.HtmlDocument]::new()
+        $HtmlDoc.LoadHtml($Results.RawContent.ToString())
+        $NextPage = $HtmlDoc.GetElementbyId("ctl00_catalogBody_nextPage")
+        $EventArgument = $HtmlDoc.GetElementbyId("__EVENTARGUMENT")[0].Attributes["value"].Value
+        $EventValidation = $HtmlDoc.GetElementbyId("__EVENTVALIDATION")[0].Attributes["value"].Value
+        $ViewState = $HtmlDoc.GetElementbyId("__VIEWSTATE")[0].Attributes["value"].Value
+        $ViewStateGenerator = $HtmlDoc.GetElementbyId("__VIEWSTATEGENERATOR")[0].Attributes["value"].Value
+        $Table = $HtmlDoc.GetElementbyId("ctl00_catalogBody_updateMatches")
+        $Rows = $Table.SelectNodes("tr")
 
-    $Output = foreach ($Row in $Rows[1..($Rows.Count - 1)]) {
-        $Cells = $Row.SelectNodes("td")
-        $Title = $Cells[1].innerText.Trim()
-        if ($Strict) {
-            if ($Title -like "*$Search*") {
+        $Output = foreach ($Row in $Rows[1..($Rows.Count - 1)]) {
+            $Cells = $Row.SelectNodes("td")
+            $Title = $Cells[1].innerText.Trim()
+            if ($Strict) {
+                if ($Title -like "*$Search*") {
+                    [PSCustomObject] @{
+                        PSTypeName = "MSCatalogUpdate"
+                        Title = $Title
+                        Products = $Cells[2].innerText.Trim()
+                        Classification = $Cells[3].innerText.Trim()
+                        LastUpdated = (Invoke-ParseDate -DateString $Cells[4].innerText.Trim())
+                        Version = $Cells[5].innerText.Trim()
+                        Size = $Cells[6].SelectNodes("span")[0].InnerText
+                        SizeInBytes = [Int] $Cells[6].SelectNodes("span")[1].InnerText 
+                        Guid = $Cells[7].SelectNodes("input")[0].Id
+                    }
+                }
+            } else {
                 [PSCustomObject] @{
                     PSTypeName = "MSCatalogUpdate"
                     Title = $Title
@@ -111,36 +126,31 @@ function Get-MSCatalogUpdate {
                     Guid = $Cells[7].SelectNodes("input")[0].Id
                 }
             }
-        } else {
-            [PSCustomObject] @{
-                PSTypeName = "MSCatalogUpdate"
-                Title = $Title
-                Products = $Cells[2].innerText.Trim()
-                Classification = $Cells[3].innerText.Trim()
-                LastUpdated = (Invoke-ParseDate -DateString $Cells[4].innerText.Trim())
-                Version = $Cells[5].innerText.Trim()
-                Size = $Cells[6].SelectNodes("span")[0].InnerText
-                SizeInBytes = [Int] $Cells[6].SelectNodes("span")[1].InnerText 
-                Guid = $Cells[7].SelectNodes("input")[0].Id
+        }
+        $Output | Sort-Object -Property LastUpdated -Descending
+
+        # If $NextPage is $null then there are more pages to collect.
+        if ($null -eq $NextPage) {
+            if ($AllPages) {
+                $NextParams = @{
+                    Search = $Search
+                    AllPages = $true
+                    EventArgument = $EventArgument
+                    EventTarget = 'ctl00$catalogBody$nextPageLinkText'
+                    EventValidation = $EventValidation
+                    ViewState = $ViewState
+                    ViewStateGenerator = $ViewStateGenerator
+                    Method = "Post"
+                }
+                Get-MSCatalogUpdate @NextParams
             }
         }
-    }
-    $Output | Sort-Object -Property LastUpdated -Descending
-
-    # If $NextPage is $null then there are more pages to collect.
-    if ($null -eq $NextPage) {
-        if ($AllPages) {
-            $NextParams = @{
-                Search = $Search
-                AllPages = $true
-                EventArgument = $EventArgument
-                EventTarget = 'ctl00$catalogBody$nextPageLinkText'
-                EventValidation = $EventValidation
-                ViewState = $ViewState
-                ViewStateGenerator = $ViewStateGenerator
-                Method = "Post"
-            }
-            Get-MSCatalogUpdate @NextParams
+    } catch {
+        $NoResults = $HtmlDoc.GetElementbyId("ctl00_catalogBody_noResultText")
+        if (-not ($null -eq $NoResults)) {
+            Write-Warning "$($NoResults.InnerText)'$Search'"
+        } else {
+            throw $_
         }
     }
 }
