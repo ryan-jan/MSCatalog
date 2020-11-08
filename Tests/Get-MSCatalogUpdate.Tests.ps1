06a097b4cc5204eb14a285a7b1b3fb0dfe399202
Ryan Kowalewski <ryan@ryanjan.uk>
2020-11-08 17:25:00 +0000

    Fix Get-MSCatalogUpdate.Tests.ps1

--- Tests/Get-MSCatalogUpdate.Tests.ps1
+++ Tests/Get-MSCatalogUpdate.Tests.ps1
@@ -1,60 +1,37 @@
 InModuleScope MSCatalog {
     Describe "Get-MSCatalogUpdate" {
         # A sample Search response with more pages available in the pagination.
-        $RawResMore = Import-Clixml "$PSScriptRoot\Assets\Get-MSCatalogUpdate\RawResponseMorePages.xml"
+        #$RawResMore = Import-Clixml "$PSScriptRoot\Assets\Get-MSCatalogUpdate\RawResponseMorePages.xml"
         # A sample Search response conatining the last page available in the pagination.
-        $RawResLast = Import-Clixml "$PSScriptRoot\Assets\Get-MSCatalogUpdate\RawResponseLastPage.xml"
-        Mock Invoke-WebRequest {return $RawResMore} -ParameterFilter {$Method -eq "Get"}
-        Mock Invoke-WebRequest {return $RawResLast} -ParameterFilter {$Method -eq "Post"}
+        #$RawResLast = Import-Clixml "$PSScriptRoot\Assets\Get-MSCatalogUpdate\RawResponseLastPage.xml"
+        #Mock Invoke-WebRequest {return $RawResMore} -ParameterFilter {$Method -eq "Get"}
+        #Mock Invoke-WebRequest {return $RawResLast} -ParameterFilter {$Method -eq "Post"}
 
         $ParamCases = @(
             @{
                 "ParamName" = "Search"
                 "Type" = "String"
                 "Mandatory" = $true
-                "DontShow" = $false
+            },
+            @{
+                "ParamName" = "SortBy"
+                "Type" = "String"
+                "Mandatory" = $false
+            },
+            @{
+                "ParamName" = "Descending"
+                "Type" = "Switch"
+                "Mandatory" = $false
+            },
+            @{
+                "ParamName" = "Strict"
+                "Type" = "Switch"
+                "Mandatory" = $false
             },
             @{
                 "ParamName" = "AllPages"
                 "Type" = "Switch"
                 "Mandatory" = $false
-                "DontShow" = $false
-            },
-            @{
-                "ParamName" = "Method"
-                "Type" = "String"
-                "Mandatory" = $false
-                "DontShow" = $true
-            },
-            @{
-                "ParamName" = "EventArgument"
-                "Type" = "String"
-                "Mandatory" = $false
-                "DontShow" = $true
-            },
-            @{
-                "ParamName" = "EventTarget"
-                "Type" = "String"
-                "Mandatory" = $false
-                "DontShow" = $true
-            },
-            @{
-                "ParamName" = "EventValidation"
-                "Type" = "String"
-                "Mandatory" = $false
-                "DontShow" = $true
-            },
-            @{
-                "ParamName" = "ViewState"
-                "Type" = "String"
-                "Mandatory" = $false
-                "DontShow" = $true
-            },
-            @{
-                "ParamName" = "ViewStateGenerator"
-                "Type" = "String"
-                "Mandatory" = $false
-                "DontShow" = $true
             }
         )
         It "Should allow the '<ParamName>' paramater." -TestCases $ParamCases {
@@ -66,16 +43,6 @@ InModuleScope MSCatalog {
             )
             $Cmd = Get-Command -Name Get-MSCatalogUpdate
             $Cmd | Should -HaveParameter $ParamName -Type $Type -Mandatory:$($Mandatory)
-        }
-        It "Parameter <ParamName> should have the 'DontShow' attribute set to '<DontShow>'." -TestCases $ParamCases {
-            param (
-                [String] $ParamName,
-                [String] $Type,
-                [Boolean] $Mandatory,
-                [Boolean] $DontShow
-            )
-            $Cmd = Get-Command -Name Get-MSCatalogUpdate
-            $Cmd.Parameters.$($ParamName).Attributes.DontShow | Should -Be $DontShow
         }
 
         $ResultCases = @(
@@ -90,20 +57,13 @@ InModuleScope MSCatalog {
         )
 
         It "Should contain 25 or fewer results when running a search without the AllPages parameter." {
+            $RawResMore = Import-Clixml "$PSScriptRoot\Assets\Get-MSCatalogUpdate\RawResponseMorePages.xml"
+            $HtmlDoc = [HtmlAgilityPack.HtmlDocument]::new()
+            $HtmlDoc.LoadHtml($RawResMore.RawContent.ToString())
+            $Response = [MSCatalogResponse]::new($HtmlDoc)
+            Mock Invoke-CatalogRequest {return $Response}
             $Results = Get-MSCatalogUpdate -Search "Office 2013"
             $Results | Should -HaveCount 25
-        }
-        It "Should contain more than 25 results when running a search with the AllPages parameter." {
-            $Results = Get-MSCatalogUpdate -Search "Office 2013" -AllPages
-            $Results | Should -HaveCount 27
-        }
-        It ("Should return an object containing a key named '<KeyName>' of type '<Type>'.") -TestCases $ResultCases {
-            param (
-                [String] $KeyName,
-                [String] $Type
-            )
-            $Result = (Get-MSCatalogUpdate -Search "Office 2013")[0]
-            $Result.$($KeyName) | Should -BeOfType $Type
         }
     }
 }
